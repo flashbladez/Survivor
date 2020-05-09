57c5ae73b25ecee7a0c905b7d69586390469f8f9
Flashbladez <s_cardwell@btinternet.com>
2020-05-09 11:31:36 +0100

    updated fighter and mover scripts

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Survivor.Movement;
 
 namespace Survivor.Combat{
-    public class Fighter:MonoBehaviour
+    public class Fighter : MonoBehaviour
     {
-        public void Attack(CombatTarget target)
+        [SerializeField] float weaponRange = 2f;
+        Transform target;
+
+        private void Update()
         {
-            print("Take That");
+
+            //remove for keyboard control
+            bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
+            if (target != null && !isInRange)
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Stop();
+            }
+        }
+
+        //remove for keyboard control
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
         }
     }
 }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -8,9 +8,10 @@ namespace Survivor.Movement{
 
     public class Mover : MonoBehaviour
     {
+        NavMeshAgent navMeshAgent;
         void Start()
         {
-            
+            navMeshAgent = GetComponent<NavMeshAgent>();
         }
     
         void Update()
@@ -20,13 +21,21 @@ namespace Survivor.Movement{
 
         public void MoveTo(Vector3 destination)
         {
-            GetComponent<NavMeshAgent>().destination = destination;//remove for keyboard control
+            //remove for keyboard control
+            navMeshAgent.destination = destination;//remove for keyboard control
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Stop()
+        {
+            //remove for keyboard control
+            navMeshAgent.isStopped = true;
         }
 
         void UpdateAnimator()
         {
             //remove first three lines for keyboard control
-            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 velocity = navMeshAgent.velocity;
             Vector3 localVelocity = transform.InverseTransformDirection(velocity);
             float speed = localVelocity.z;
             GetComponent<Animator>().SetFloat("forwardSpeed", speed);
