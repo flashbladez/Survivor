8e1a0614f4c7086ddd89dff2d72f89b878a289d1
Flashbladez <s_cardwell@btinternet.com>
2020-05-29 19:02:12 +0100

    portal  loads next scene

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Survivor.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+           
+        }
+    }
+}
