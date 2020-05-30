be17ea46a227763cf896cbf6781bce7266436a52
Flashbladez <s_cardwell@btinternet.com>
2020-05-30 18:33:27 +0100

    Can now use portals

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,21 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.AI;
 namespace Survivor.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
 
         void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
            
+        }
+
+        IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+           // print("SceneLoaded");
+            Destroy(gameObject);
+        }
+
+        void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        Portal GetOtherPortal()
+        {
+           foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if(portal == this)
+                {
+                    continue;
+                }
+                return portal;
+            }
+            return null;
         }
     }
 }
