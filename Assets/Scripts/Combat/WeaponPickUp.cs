be99b1de41e0e3885ce057c0f3e66b9cf15cd075
Flashbladez <s_cardwell@btinternet.com>
2020-06-18 20:18:29 +0100

    colectable Objects appear to respawn

--- Assets/Scripts/Combat/WeaponPickUp.cs
+++ Assets/Scripts/Combat/WeaponPickUp.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace Survivor.Combat
@@ -6,15 +7,35 @@ namespace Survivor.Combat
     public class WeaponPickUp : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 5f;
 
         void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Player")
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
             }
            
         }
+
+        IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickUp(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickUp(true);
+        }
+
+        void ShowPickUp(bool shouldShow)
+        {
+            GetComponent<Collider>().enabled = shouldShow;
+           
+            foreach(Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
+            }
+        }
+
+       
     }
 }
