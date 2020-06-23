87630c175f1d1d7938f60578451ba257f9ebb404
Flashbladez <s_cardwell@btinternet.com>
2020-06-23 18:50:41 +0100

    Added level and level display

---
+++ Assets/Scripts/Stats/LevelDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Survivor.Stats
+{
+    public class LevelDisplay : MonoBehaviour
+    {
+        BaseStats baseStats;
+
+        void Awake()
+        {
+            baseStats = GameObject.FindWithTag("Player").GetComponent<BaseStats>();
+        }
+
+
+        void Update()
+        {
+            GetComponent<Text>().text = String.Format("{0:0}" ,baseStats.Getlevel());
+        }
+    }
+}
