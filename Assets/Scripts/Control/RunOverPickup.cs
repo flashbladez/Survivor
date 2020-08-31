c4a1a72204d8b4f0c05aace9370b81d7142828e7
Flashbladez <s_cardwell@btinternet.com>
2020-08-31 11:35:45 +0100

    Added Run Over pickups

---
+++ Assets/Scripts/Control/RunOverPickup.cs
@@ -0,0 +1,20 @@
+﻿using GameDevTV.Inventories;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Survivor.Control
+{
+    [RequireComponent(typeof(Pickup))]
+    public class RunOverPickup : MonoBehaviour
+    {
+
+        void OnTriggerEnter(Collider other)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if(other.gameObject == player)
+            {
+                GetComponent<Pickup>().PickupItem();
+            }
+        }
+    }
+}
