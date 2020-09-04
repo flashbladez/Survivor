486a37812c922176ed5c7ea454d679fed43a5f45
Flashbladez <s_cardwell@btinternet.com>
2020-09-04 13:34:56 +0100

    ActionItem now knows which item is used on Action Bar

--- Assets/GameDev.tv Assets/Scripts/Inventories/ActionItem.cs
+++ Assets/GameDev.tv Assets/Scripts/Inventories/ActionItem.cs
@@ -1,8 +1,16 @@
+using Survivor.Attributes;
 using System;
 using UnityEngine;
 
 namespace GameDevTV.Inventories
 {
+    public enum ActionItemType
+    {
+        Spell,
+        Healing,
+        Mana,
+        Scroll
+    }
     /// <summary>
     /// An inventory item that can be placed in the action bar and "Used".
     /// </summary>
@@ -10,13 +18,14 @@ namespace GameDevTV.Inventories
     /// This class should be used as a base. Subclasses must implement the `Use`
     /// method.
     /// </remarks>
+    /// 
     [CreateAssetMenu(menuName = ("GameDevTV/GameDevTV.UI.InventorySystem/Action Item"))]
     public class ActionItem : InventoryItem
     {
         // CONFIG DATA
         [Tooltip("Does an instance of this item get consumed every time it's used.")]
         [SerializeField] bool consumable = false;
-
+        [SerializeField] ActionItemType actionItemType;
         // PUBLIC
 
         /// <summary>
@@ -25,7 +34,16 @@ namespace GameDevTV.Inventories
         /// <param name="user">The character that is using this action.</param>
         public virtual void Use(GameObject user)
         {
-      
+
+            if (actionItemType.ToString() == "Healing")
+            {
+                user.GetComponent<Health>().Heal(20f);
+                Debug.Log(actionItemType + " OK");
+            }
+            if (actionItemType.ToString() == "Mana")
+            {
+                Debug.Log(actionItemType + " OK");
+            }
             Debug.Log("Using action: " + this);
         }
 
